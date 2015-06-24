ba7a8fd811451f2e66053add61262161a9983481
Rusyuu <samuel.carnes@colorado.edu>
2015-06-24 16:29:32 -0600

    cgi-bin updating

--- cgi-bin/dataImportTesting.py
+++ cgi-bin/dataImportTesting.py
@@ -1,3 +1,40 @@
 #!/usr/bin/env python
 
 import json
+
+print "Content-type: text/html"
+print
+
+print '''
+
+<html>
+    <head>
+    <title>MaxDonald's</title>
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="http://jquery-csv.googlecode.com/git/src/jquery.csv.js"></script>
+
+        <script>
+
+        $( document ).ready(function() {
+        '''
+
+        
+	'''	
+	</script>
+    
+	<style>
+	body {
+		background-color: #DD1021;
+		
+	}
+	
+	</style>
+    
+    </head>
+    
+    <body>
+    </body>
+</html>
+'''
+
+
